2f168b2378470dc19670633fef0efcaf45daa692
Celian RAIMBAULT <celian.raimbault@epita.fr>
2020-10-06 10:41:08 +0200

    Updated error.h (prints file and line)

--- src/error.h
+++ src/error.h
@@ -7,9 +7,11 @@
 #include <stdlib.h>
 
 // Fatal error, prints MSG to stderr and exit(-1)
-#define ERROR(MSG) { fprintf(stderr, "ERROR : %s\n", (MSG)); exit(-1); }
+#define ERROR(MSG) { \
+    fprintf(stderr, "ERROR %s:%d : %s\n", __FILE__, __LINE__, (MSG)); \
+    exit(-1); }
 
 // If false, raises an error
-#define ASSERT(PRED, MSG) { if (!(PRED)) ERROR(MSG); }
+#define ASSERT(PRED, MSG) { if (!(PRED)) ERROR((MSG)); }
 
 #endif // ERROR_H
