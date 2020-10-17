eaab3fe7c1aea4819621b65c44b6cfadb30d9305
Celian RAIMBAULT <celian.raimbault@epita.fr>
2020-10-17 15:53:36 +0200

    Added losses with MSE loss

--- src/main.c
+++ src/main.c
@@ -3,6 +3,7 @@
 #include <time.h>
 #include "matrix.h"
 #include "layers.h"
+#include "losses.h"
 #include "initializer.h"
 
 int main(int __attribute__((unused)) argc,
@@ -14,19 +15,26 @@ int main(int __attribute__((unused)) argc,
     const float xData[] = {
             0, 1,
         };
+    const float yData[] = {
+            1
+        };
     Matrix *x = matrixCreate(2, 1, xData);
+    Matrix *y = matrixCreate(1, 1, yData);
 
     // Network
-    Layer *fc = denseNew(2, 4);
+    Layer *fc = denseNew(2, 1);
     Layer *activation = sigmoidNew();
+    LossFunction criterion = mseLoss;
 
     // Feed forward
     Matrix *y1 = layerForward(fc, x, true);
     Matrix *y2 = layerForward(activation, y1, true);
 
+    Loss *error = criterion(y2, y);
+    printf("Loss : %g\n", error->loss);
+
     // Back propagate
-    // TODO : Real loss
-    Matrix *dLoss = matrixLike(y2);
+    Matrix *dLoss = error->grad;
     Matrix *dLossDY2 = layerBackward(activation, dLoss);
     Matrix *dLossDY1 = layerBackward(fc, dLossDY2);
 
@@ -38,27 +46,28 @@ int main(int __attribute__((unused)) argc,
     matrixPrint(dLossDY1);
 
     // Free (each epoch)
+    lossFree(error);
     matrixFree(dLossDY1);
     matrixFree(dLossDY2);
-    matrixFree(dLoss);
     matrixFree(y2);
     matrixFree(y1);
 
     // Free (each phase)
+    matrixFree(y);
     matrixFree(x);
 
     // Free network
     layerFree(activation);
 
-    // Create test matrices
-    Matrix *a = randNormal(2, 3);
-    Matrix *b = randNormal(3, 2);
-    Matrix *c = randNormal(3, 3);
+    // // Create test matrices
+    // Matrix *a = randNormal(2, 3);
+    // Matrix *b = randNormal(3, 2);
+    // Matrix *c = randNormal(3, 3);
 
-    // Display results
-    matrixPrint(a);
-    matrixPrint(b);
-    matrixPrint(c);
+    // // Display results
+    // matrixPrint(a);
+    // matrixPrint(b);
+    // matrixPrint(c);
 
     // // Display 5000 values created with randomFloatNorm() to verify normal distribution
     // float list[5000];
