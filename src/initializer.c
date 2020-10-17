831a1cfa0d4365d5bfcb4bcda13d033ad8e207bd
raphael.brenn <raphael.brenn@epita.fr>
2020-10-17 15:37:06 +0200

    Replaced tabs with spaces

--- src/initializer.c
+++ src/initializer.c
@@ -13,20 +13,20 @@ float randFloatNorm() {
     // Method described by Abramowitz and Stegun to generate random numbers
     // with a gaussian distribution
 
-	static float U, V;
-	static int phase = 0;
-	float Z;
+    static float U, V;
+    static int phase = 0;
+    float Z;
 
-	if(phase == 0) {
-		U = (rand() + 1.) / (RAND_MAX + 2.);
-		V = rand() / (RAND_MAX + 1.);
-		Z = sqrt(-2 * log(U)) * sin(2 * PI * V);
-	} else
-		Z = sqrt(-2 * log(U)) * cos(2 * PI * V);
+    if(phase == 0) {
+        U = (rand() + 1.) / (RAND_MAX + 2.);
+        V = rand() / (RAND_MAX + 1.);
+        Z = sqrt(-2 * log(U)) * sin(2 * PI * V);
+    } else
+        Z = sqrt(-2 * log(U)) * cos(2 * PI * V);
 
-	phase = 1 - phase;
+    phase = 1 - phase;
 
-	return Z;
+    return Z;
 }
 
 Matrix *randUniform(size_t rows, size_t cols, float min, float max) {
