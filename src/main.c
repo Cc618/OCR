f8574e73ddfddb2e9b82c82be1c974e9bfc3b669
Celian RAIMBAULT <celian.raimbault@epita.fr>
2020-10-22 18:34:49 +0200

    Added xor network and main net function in main.c

--- src/main.c
+++ src/main.c
@@ -1,117 +1,150 @@
 #include <stdio.h>
 #include <math.h>
 #include <time.h>
+#include <string.h>
 #include "matrix.h"
 #include "layers.h"
 #include "losses.h"
 #include "initializer.h"
 #include "optimizer.h"
 
-int main(int __attribute__((unused)) argc,
-        char __attribute__((unused)) **argv) {
-    srand(time(NULL));
+void imgMain() {
+    // TODO
+}
 
+void netMain() {
     // Hyper parameters
-    float learningRate = 5e-2;
-    unsigned int batchSize = 4;
+    static const float learningRate = 1e-2;
+    static const unsigned int batchSize = 4;
+    static const size_t epochs = 1500;
+    static const size_t dispFreq = 100;
 
     // Dataset
     const float xData[] = {
+            0, 0,
             0, 1,
+            1, 0,
+            1, 1,
         };
     const float yData[] = {
-            1
+            0,
+            1,
+            1,
+            0,
         };
-    Matrix *x = matrixCreate(2, 1, xData);
-    Matrix *y = matrixCreate(1, 1, yData);
+    Matrix *xs[batchSize];
+    Matrix *ys[batchSize];
+
+    for (size_t i = 0; i < batchSize; ++i) {
+        xs[i] = matrixCreate(2, 1, &xData[2 * i]);
+        ys[i] = matrixCreate(1, 1, &yData[i]);
+    }
 
     // Network
-    Layer *fc = denseNew(2, 1);
-    Layer *activation = sigmoidNew();
+    Layer *fc1 = denseNew(2, 4);
+    Layer *activation1 = sigmoidNew();
+    Layer *fc2 = denseNew(4, 1);
+    // TODO Layer *activation2 = sigmoidNew();
     LossFunction criterion = mseLoss;
     Optimizer *opti = sgdNew(learningRate, batchSize);
 
-    static const unsigned long long epochs = 100ull;
+    // Just to display it at the end
+    Matrix *display[batchSize];
 
-    for (unsigned long long e = 1; e <= epochs; ++e) {
-        // Feed forward
-        Matrix *y1 = layerForward(fc, x, true);
-        Matrix *y2 = layerForward(activation, y1, true);
+    for (size_t e = 1; e <= epochs; ++e) {
+        float avgLoss = 0;
 
-        Loss *error = criterion(y2, y);
+        // For each batch
+        for (size_t batch = 0; batch < batchSize; ++batch) {
+            Matrix *x = xs[batch];
+            Matrix *y = ys[batch];
 
-        // Back propagate
-        Matrix *dLoss = error->grad;
-        Matrix *dLossDY2 = layerBackward(activation, dLoss);
-        Matrix *dLossDY1 = layerBackward(fc, dLossDY2);
+            // Feed forward
+            Matrix *y1 = layerForward(fc1, x, true);
+            Matrix *y2 = layerForward(activation1, y1, true);
+            Matrix *y3 = layerForward(fc2, y2, true);
 
-        // Optimize (each batch)
-        layerUpdate(fc, opti);
-        layerUpdate(activation, opti);
+            // Save to display
+            if (e == epochs)
+                display[batch] = matrixCopy(y3);
+
+            Loss *error = criterion(y3, y);
+
+            // Back propagate
+            Matrix *dLoss = error->grad;
+            Matrix *dLossDY3 = layerBackward(fc2, dLoss);
+            Matrix *dLossDY2 = layerBackward(activation1, dLossDY3);
+            Matrix *dLossDY1 = layerBackward(fc1, dLossDY2);
+
+            avgLoss += error->loss;
+
+            // Free (each epoch)
+            lossFree(error);
+            matrixFree(dLossDY1);
+            matrixFree(dLossDY2);
+            matrixFree(dLossDY3);
+            matrixFree(y3);
+            matrixFree(y2);
+            matrixFree(y1);
+        }
+
+        avgLoss /= batchSize;
 
         // Display result
-        // matrixPrint(x);
-        printf("Epoch %3llu", e);
-        printf(", Loss : %.3e", error->loss);
-        printf(", Prediction : ");
-        matrixPrint(y2);
-        // puts("> Last gradient :");
-        // matrixPrint(dLossDY1);
-        // puts("> Updated gradient (should be 0) :");
-        // matrixPrint(((Dense*)fc)->gradWeight);
+        if (e % dispFreq == 0) {
+            // matrixPrint(x);
+            printf("Epoch %4lu", e);
+            printf(", Loss : %.1e\n", avgLoss);
+            // printf(", Prediction : ");
+            // matrixPrint(y3);
+        }
 
-        // Free (each epoch)
-        lossFree(error);
-        matrixFree(dLossDY1);
-        matrixFree(dLossDY2);
-        matrixFree(y2);
-        matrixFree(y1);
+        // Optimize (each batch)
+        layerUpdate(fc2, opti);
+        layerUpdate(activation1, opti);
+        layerUpdate(fc1, opti);
     }
 
-    // Free (each phase)
-    matrixFree(y);
-    matrixFree(x);
+    // Show results
+    for (size_t i = 0; i < batchSize; ++i) {
+        printf("x = %.2f %.2f\n", xs[i]->data[0], xs[i]->data[1]);
+        printf("y = %.2f (should be %.2f)\n\n", display[i]->data[0], ys[i]->data[0]);
+    }
+
+    // Free batch (each phase)
+    for (size_t i = 0; i < batchSize; ++i) {
+        matrixFree(display[i]);
+        matrixFree(xs[i]);
+        matrixFree(ys[i]);
+    }
 
     // Free network
-    layerFree(fc);
-    layerFree(activation);
+    layerFree(fc1);
+    layerFree(fc2);
+    layerFree(activation1);
     optimizerFree(opti);
+}
 
-    // // Create test matrices
-    // Matrix *a = randNormal(2, 3);
-    // Matrix *b = randNormal(3, 2);
-    // Matrix *c = randNormal(3, 3);
+int main(int argc,
+        char **argv) {
+    // Initialization
+    srand(time(NULL));
 
-    // // Display results
-    // matrixPrint(a);
-    // matrixPrint(b);
-    // matrixPrint(c);
+    int err = 0;
+    if (argc != 2)
+        err = 1;
+    else if (strcmp(argv[1], "img") == 0)
+        imgMain();
+    else if (strcmp(argv[1], "net") == 0)
+        netMain();
+    else
+        err = 1;
 
-    // // Display 5000 values created with randomFloatNorm() to verify normal distribution
-    // float list[5000];
-    // for (int i=0; i<5000; i++) {
-    //     list[i] = randFloatNorm();
-    // }
-    // for (float k= -3; k < 3; k+=0.1) {
-    //     int count = 0;
-    //     for (int j = 0; j < 5000; j++) {
-    //         float current = list[j];
-    //         if (k < current && current < k+0.1) {
-    //             count += 1;
-    //         }
-    //     }
-    //     for (int l = 0; l < count; l++) {
-    //         printf("=");
-    //     }
-    //     printf("\n");
-    // }
-
-    // // Free test matrices
-    // matrixFree(a);
-    // matrixFree(b);
-    // matrixFree(c);
-    // layerFree(fc);
+    if (err) {
+        puts("usage:");
+        puts("    ocr img   Run image processing demo");
+        puts("    ocr net   Run neural network demo");
+    }
 
     return 0;
 }
-
